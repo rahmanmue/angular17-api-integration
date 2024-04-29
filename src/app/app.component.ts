1f95a06362814124559ca0f095a85b5024174990
rahman.muraman <rahman.muraman@gmail.com>
2024-04-29 07:53:51 +0700

    refactor: change app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HouseService } from './services/house.service';
 import { Houses } from './interfaces/houses';
-import { ModaleditComponent } from './components/modaledit/modaledit.component';
-import { CurrencyPipe } from '@angular/common';
+import { CardHouseComponent } from './components/card-house/card-house.component';
+import { ModalAddComponent } from './components/modal-add/modal-add.component';
+import { ModalEditComponent } from './components/modal-edit/modal-edit.component';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ReactiveFormsModule, ModaleditComponent, CurrencyPipe],
+  imports: [
+    CardHouseComponent, 
+    ModalAddComponent, 
+    ModalEditComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'app-crud';
   houses : Houses[] = []
+  itemEdit! : Houses;
   error! : string;
   completed : boolean = false;
 
@@ -26,55 +32,46 @@ export class AppComponent {
     this.getHouses()
   }
 
+  onClickEdit(item: Houses){
+    this.itemEdit = item;
+  }
+
   getHouses(){
     this.houseService.getHouses().subscribe({
       next: data => this.houses = data,
       error : err => this.error = err,
       complete :() => this.completed = true,
     })
-  }   
+  }  
 
-  onClickDelete(id:any){
+  addData(addDataForm: FormGroup){
+    console.log(addDataForm);
+    if(!addDataForm?.valid){
+      alert("Form is not valid")
+      return
+    }else{
+      console.log(addDataForm.value)
+      this.houseService.postHouse(addDataForm.value).subscribe();
+      this.getHouses();
+      addDataForm.reset();
+    }
+  }
+
+  editData(editDataForm:FormGroup){
+    let id : number = editDataForm.value.id as number;
+    this.houseService.putHouse(id, editDataForm.value).subscribe({
+      next: data => console.log(data),
+      error: err => console.log(err)
+    });
+    this.getHouses();
+  }
+
+  deleteData(id:number){
     this.houseService.deleteHouse(id).subscribe({
       next: data => console.log(data),
       error: err => console.log(err)
     });
     this.getHouses()
-  }
-
-  itemEdit! : Houses;
-
-  onClickEdit(item: Houses){
-    this.itemEdit = item;
-    console.log(this.itemEdit)
-  }
-
-  addDataForm = new FormGroup({
-    name: new FormControl('', Validators.required),
-    location: new FormControl('', Validators.required),
-    roomAvailable : new FormControl('', Validators.required),
-    price : new FormControl('', Validators.required),
-    imageLink : new FormControl('', Validators.required)
-  })
-
-  addData(){
-    if(!this.addDataForm.valid){
-      alert("Form is not valid")
-      return
-    }else{
-      console.log(this.addDataForm.value)
-      this.houseService.addHouse(this.addDataForm.value).subscribe();
-      this.getHouses();
-      this.addDataForm.reset();
-    }
-  }
-
-  editData(id:number, body:any){
-    this.houseService.updateHouse(id, body).subscribe({
-      next: data => console.log(data),
-      error: err => console.log(err)
-    });
-    this.getHouses();
   }
 
 }
